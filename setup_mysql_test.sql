fe1e302ecea4004b059f46f8cb9f7c719c225029
unknown <d.mwape@alustudent.com>
2024-02-01 19:48:20 +0000

    number 22

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,5 @@
--- Create a database if it doesn't already exist
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Create a user if it doesn't already exist and set its password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
